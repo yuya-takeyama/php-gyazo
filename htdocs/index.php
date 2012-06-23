d2439d1d5a7e4fc2f1dfc621ecf2432a9e5d0f8a
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2012-06-24 01:14:40 +0900

    Add uploading.

--- htdocs/index.php
+++ htdocs/index.php
@@ -10,6 +10,8 @@ require_once 'Phat/Application.php';
 
 class GyazoApp extends Phat_Application
 {
+    const MD5_LENGTH = 32;
+
     public function picture($hash)
     {
         $record = $this->getRecordByHash($hash);
@@ -29,21 +31,50 @@ class GyazoApp extends Phat_Application
         }
     }
 
+    public function upload()
+    {
+        $file = $_FILES['imagedata']['tmp_name'];
+        $body = file_get_contents($file);
+        $hash = md5($body);
+        $stmt = $this['db']->prepare(
+            'INSERT INTO pictures (`hash`, `body`, `created_at`, `updated_at`) ' .
+            'VALUES (?, ?, NOW(), NOW())'
+        );
+        $stmt->bindParam(1, $hash, PDO::PARAM_STR, self::MD5_LENGTH);
+        $stmt->bindParam(2, $body, PDO::PARAM_LOB);
+        if ($stmt->execute()) {
+            $this->response->write($this->getImageUrl($hash));
+        } else {
+            $this->halt(500, "Failed to upload " . json_encode($stmt->errorInfo()));
+        }
+    }
+
     private function getRecordByHash($hash)
     {
         $stmt = $this['db']->prepare(
-            'SELECT hash, body, created_at, updated_at FROM pictures WHERE hash = :hash'
+            'SELECT hash, body, created_at, updated_at FROM pictures WHERE hash = ?'
         );
-        $stmt->bindParam(':hash', $hash);
+        $stmt->bindParam(1, $hash);
         $stmt->execute();
         return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    private function getImageUrl($hash)
+    {
+        $env = $this->environment;
+        $scheme = isset($env['HTTPS']) && $env['HTTPS'] === 'on' ? 'https' : 'http';
+        $host = $env['SERVER_NAME'];
+        return "{$scheme}://{$host}/{$hash}";
     }
 }
 
 $app = new GyazoApp;
-$app['db'] = new PDO('mysql:dbname=php_gyazo_dev;host=localhost', 'gyazo', 'gyazo');
+$app['db'] = new PDO('mysql:dbname=php_gyazo_dev;host=localhost', 'gyazo', 'gyazo', array(
+    PDO::MYSQL_ATTR_DIRECT_QUERY => true,
+));
 
 $app->get('/:hash.png', array($app, 'picture'));
 $app->get('/:hash', array($app, 'picturePage'));
+$app->post('/upload.cgi', array($app, 'upload'));
 
 $app->run();
