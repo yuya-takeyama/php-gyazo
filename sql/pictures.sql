bd215da551c0af844c5d0ccdb62aace677436496
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2012-06-24 03:32:52 +0900

    Fix to save user_id.

--- sql/pictures.sql
+++ sql/pictures.sql
@@ -1,9 +1,11 @@
 CREATE TABLE `pictures` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `hash` varchar(32) COLLATE utf8_bin NOT NULL,
+  `user_id` varchar(32) COLLATE utf8_bin NOT NULL,
   `body` mediumblob NOT NULL,
   `created_at` datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
   `updated_at` datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `hash` (`hash`)
+  UNIQUE KEY `hash` (`hash`),
+  INDEX `user_id` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
