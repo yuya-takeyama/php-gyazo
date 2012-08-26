40cd33a0f0c4745eb8c65a20aa289cec282357fa
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2012-08-27 00:33:39 +0900

    Add file upload testing

--- tests/PhpGyazo/Functional/ApplicationTest.php
+++ tests/PhpGyazo/Functional/ApplicationTest.php
@@ -1,13 +1,16 @@
 <?php
 class PhpGyazo_Tests_Functional_ApplicationTest extends Sumile_WebTestCase
 {
+    const FIXTURE_FILE     = 'tests/fixtures/picture.png';
+    const FIXTURE_FILE_MD5 = 'fbe459130020268492b84eaec1bf8101';
+
     private $db;
 
     public function setUp()
     {
         parent::setUp();
 
-        $this->db = new PDO('mysql:host=localhost', 'root');
+        $this->db = new PDO('mysql:dbname=php_gyazo_test;host=localhost', 'root');
 
         $this->db->exec('DROP TABLE IF EXISTS pictures');
         $this->db->exec(file_get_contents('sql/pictures.sql'));
@@ -26,6 +29,34 @@ class PhpGyazo_Tests_Functional_ApplicationTest extends Sumile_WebTestCase
         $res = $this->get('/');
 
         $this->assertTrue($res->isNotFound());
+    }
+
+    /**
+     * @test
+     */
+    public function picture_upload_returns_page_url_when_succeeded()
+    {
+        $res = $this->post('/upload.cgi', array(
+            'post'  => array(
+                'id' => 'testuser',
+            ),
+            'files' => array(
+                'imagedata' => self::FIXTURE_FILE,
+            ),
+        ));
+
+        $this->assertEquals('http://localhost/' . self::FIXTURE_FILE_MD5, $res->body());
+        $this->assertTrue($res->isOk());
+
+        $res = $this->get('/' . self::FIXTURE_FILE_MD5);
+
+        $this->assertTrue($res->isOk());
+        $this->assertContains('<img src="/' . self::FIXTURE_FILE_MD5 . '.png" alt="Picture" />', $res->body());
+
+        $res = $this->get('/' . self::FIXTURE_FILE_MD5 . '.png');
+
+        $this->assertTrue($res->isOk());
+        $this->assertEquals(file_get_contents(self::FIXTURE_FILE), $res->body());
     }
 
     /**
